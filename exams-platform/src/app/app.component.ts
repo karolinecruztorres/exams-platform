1f2e7a50ad2ce80d4b6d8fd9f5b28ded5a8c99e7
Karoline Cruz <karollcruz@hotmail.com>
2024-03-29 18:10:54 +0000

    feat: adds routes to the project

--- exams-platform/src/app/app.component.ts
+++ exams-platform/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
 import { ContainerWrapperComponent } from './container/container-wrapper/container-wrapper.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ContainerWrapperComponent],
+  imports: [
+    ContainerWrapperComponent,
+    MatToolbarModule,
+    RouterOutlet,
+    RouterLink,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- exams-platform/src/app/app.routes.ts
+++ exams-platform/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LogoComponent } from './components/logo/logo.component';
+import { NotificationsComponent } from './components/notifications/notifications.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+
+export const routes: Routes = [
+  {
+    path: 'logo',
+    title: 'Logo',
+    component: LogoComponent,
+  },
+  {
+    path: 'search-bar',
+    title: 'Search',
+    component: SearchBarComponent,
+  },
+  {
+    path: 'notifications',
+    title: 'Notifications',
+    component: NotificationsComponent,
+  },
+  {
+    path: 'profile',
+    title: 'Profile',
+    component: ProfileComponent,
+  },
+];
